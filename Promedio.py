2a9d4c2c808319ec1941e64716b7d6e20da3e037
Christopheal <81484254+Christopheal@users.noreply.github.com>
2021-08-15 19:20:07 -0600

    Create Promedio.py

---
+++ Promedio.py
@@ -0,0 +1,6 @@
+Print(“Bienvenido”)
+n1= float(input("Ingrese el primer numero"))
+n2= float(input("Ingrese el segundo numero"))
+n3= float(input("Ingrese el tercero numero"))
+prom = (n1+n2+n3)/3
+print("El promedio es ",prom,"")
